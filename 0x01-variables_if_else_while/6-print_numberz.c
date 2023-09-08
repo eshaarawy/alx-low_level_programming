f7bcf50de255c9fe4969ae36404b7747ab705714
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-08 23:54:35 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Base 10
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i < 10; i++)
+
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
