a13bc984b01b9a57a5eb791dcce83a3461b1eec8
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-09 00:05:20 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Base 16
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		putchar(i);
+	}
+	for (ch = 'a'; ch <= 'f'; i++)
+
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
