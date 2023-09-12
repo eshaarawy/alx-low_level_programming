b15136592940320a8070d9c766de3fd061016636
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-12 21:25:14 +0300

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute
+*
+* @n: number
+* Return: Always 0.
+*/
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + 48);
+			_putchar((hrs / 10) + 48);
+			_putchar(':');
+			_putchar((mins / 10) + 48);
+			_putchar((mins / 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
