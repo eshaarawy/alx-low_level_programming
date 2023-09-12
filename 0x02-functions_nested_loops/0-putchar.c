e366d335e301c01cdf3d4ad361c91347720bb22c
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-12 19:42:45 +0300

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: putchar
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
