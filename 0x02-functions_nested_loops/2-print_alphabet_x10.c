f48a14849e3c6fbda9a8e93c82c657cd8df6af1d
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-12 20:41:46 +0300

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print characters from a to z 1- times
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
