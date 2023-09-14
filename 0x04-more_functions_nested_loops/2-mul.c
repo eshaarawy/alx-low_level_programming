5672679da2050021d973f44a7990f0149d9c1907
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-14 22:08:54 +0300

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* mul - check if char is uppercase
+*
+* Description: Alphabet
+*
+* @a: first number
+* @b: second number
+*
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
