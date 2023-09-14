37c9a13d5b4cd559cb0194056858930684e2ef43
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-14 22:14:34 +0300

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* print_numbers - check if char is uppercase
+*
+* Description: Alphabet
+*
+*
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+}
