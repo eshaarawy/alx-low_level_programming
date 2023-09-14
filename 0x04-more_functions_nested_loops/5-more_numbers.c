2b4719f00048c8f10ff41724b917de02b1d5d14c
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-14 22:23:16 +0300

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* more_numbers - check if char is uppercase
+*
+* Description: Alphabet
+*
+*
+* Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+}
