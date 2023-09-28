46b157ac1f652e4e9caf24cd4d3ebedb598fdf73
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-28 13:48:51 +0900

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _pow_recursion - print factorial of a number
+* @x: number
+* @y: number
+* Return: x to the power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+return (x * _pow_recursion(x, y - 1));
+}
