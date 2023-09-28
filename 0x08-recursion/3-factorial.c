a9a9dc9e71525018f38d4ccd6dfc1f62cd8bc147
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-09-28 13:40:42 +0900

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* factorial - print factorial of a number
+* @n: number
+* Return: factorial
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+
+	return (n * (factorial(n - 1)));
+}
