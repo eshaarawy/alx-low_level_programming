f6cad0a7c06ef4a2d5b1d332d54928a3420d08e7
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-10-02 23:53:33 +0900

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints
+* @argc: int
+* @argv: ptr
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
