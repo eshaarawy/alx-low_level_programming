dece8b017f495fb118324446a6f5e81bcdf68263
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-10-04 21:29:46 +0900

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* str_concat - create array of size size and assign char c
+* @s1: ptr to first array
+* @s2: ptr to second array
+* Description: create array of size size and assign char c
+* Return: pointer to array, NULL if fail
+*
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, i;
+
+	char *ptr;
+
+	for (; s1[size1] != '\0'; size1++)
+	;
+
+	for (; s2[size2] != '\0'; size2++)
+	;
+
+	ptr = malloc(sizeof(char) * (size1 + size2 + 1));
+
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (; i < size2 + size1; i++)
+	{
+		ptr[i] = s2[i - size1];
+	}
+
+	return (ptr);
+}
