2a3acf24fdd6eaf4e0e8c25aabfcce89757bf58c
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-10-11 00:58:47 +0900

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog
+* @name: name
+* @age: age
+* @owner: owner
+*/
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+#endif;
