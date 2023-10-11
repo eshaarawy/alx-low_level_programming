99b74aa45cf28c8d4567e496df8e895741a5535f
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-10-11 21:37:40 +0900

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <function_pointers.h>
+/**
+* print_name - prints name
+* @name: string
+* @f: the printing function pointer
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
