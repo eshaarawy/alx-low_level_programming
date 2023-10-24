ad3f451bced6170baa7712026b573799c83aace8
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-10-24 23:02:48 +0900

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* add_nodeint - prints a linked lists
+* @head: pointer to first node
+* @n: data
+* Return: size of list
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->n = n;
+
+	if (*head)
+		new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
