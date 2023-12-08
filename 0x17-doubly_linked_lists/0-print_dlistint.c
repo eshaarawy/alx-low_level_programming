e06edf3693e605acf8ff831331a0771427867214
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-08 10:02:53 +0900

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_dlistint - print
+* @h: header
+*
+* Return: number or nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
